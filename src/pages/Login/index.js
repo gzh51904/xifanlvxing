e6111af3b9ef918191008a8725fb13660ebe5aed
lin <1725371533@qq.com>
2019-07-26 00:08:43 +0800

    登录注册

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,13 +8,21 @@ class Login extends Component{
     constructor(){
         super();
         this.state={
-            isstatus:true,
+            isstatus:false,
             tabs2:[
                 { 
                     title: '普通登录' 
                 },
                 { 
                     title: '手机验证码登录'
+                },
+            ],
+            tabs3:[
+                { 
+                    title: '手机号注册' 
+                },
+                { 
+                    title: '邮箱注册'
                 },
             ],
         }
@@ -33,16 +41,16 @@ class Login extends Component{
     }
     
     render(){
-        let {tabs2}=this.state;
+        let {tabs2,tabs3}=this.state;
         return<div className="Login">
             <header>
                 <div className="header">
                     <div className="left"><span>&lt;</span></div>
-                    <div className="right"><span className="loginbtn" onClick={this.change}>{this.state.isstatus?'注册':'登录'}</span></div>
+                    <div className="right"><span className="loginbtn" onClick={this.change}>{this.state.isstatus?'登录':'注册'}</span></div>
                 </div>
             </header>
             <main>
-                <div className="login_content">
+                <div className="login_content" style={{display:this.state.isstatus?'none':'block'}}>
                     <h1 className="login_title">
                         稀饭旅游账号登录
                     </h1>
@@ -54,16 +62,16 @@ class Login extends Component{
                         initialPage={0}
                         animated={false}
                         tabBarPosition="top"
-                        renderTab={tab => <span onTabClick={this.tabClick}>{tab.title}</span>}
+                        renderTab={tab => <span>{tab.title}</span>}
                         >
-                        <div className="commonLogin">
+                        <div className="commonLogin" >
                            <input className="username" text="text" placeholder="用户名/邮箱"/>
                            <input className="password" text="text" placeholder="登录密码"/>
                            <div className="forget"><div className="findBtn"><span>忘记密码</span></div></div>
                         </div>
-                        <div className="mobileLogin">
-                           <input className="username" text="text" placeholder="手机号"/>
-                           <input className="password" text="text" placeholder="验证码"/>
+                        <div className="commonLogin">
+                           <input className="logMobile" text="text" placeholder="手机号"/>
+                           <input className="mobileCode" text="text" placeholder="验证码"/>
                            <div className="code"><div className="codeBtn"><span>获取验证码</span></div></div>
                         </div>
                         </Tabs>
@@ -74,6 +82,42 @@ class Login extends Component{
 
 
                     <p className="xieyi">登录即代表您已同意我们的<span>&nbsp;服务协议</span></p>
+                </div>
+                <div className="login_content" style={{display:this.state.isstatus?'block':'none'}}>
+                    <h1 className="login_title">
+                        稀饭旅游账号注册
+                    </h1>
+                    <div className="login_form">
+
+                        <form>
+                        <WhiteSpace />
+                        <Tabs tabs={tabs3}
+                        initialPage={0}
+                        animated={false}
+                        tabBarPosition="top"
+                        renderTab={tab => <span>{tab.title}</span>}
+                        >
+                        <div className="commonLogin" >
+                           <input className="username" text="text" placeholder="手机号"/>
+                           <input className="password" text="text" placeholder="登录密码"/>
+                           <input className="mobileCode" text="text" placeholder="验证码"/>
+                           {/* <div className="forget"><div className="findBtn"><span>忘记密码</span></div></div> */}
+                           <div className="code"><div className="codeBtn"><span>获取验证码</span></div></div>
+                        </div>
+                        <div className="commonLogin">
+                           <input className="logMobile" text="text" placeholder="邮箱"/>
+                           <input className="password" text="text" placeholder="登录密码"/>
+                           <input className="mobileCode" text="text" placeholder="验证码"/>
+                           <div className="code"><div className="codeBtn"><span>获取验证码</span></div></div>
+                        </div>
+                        </Tabs>
+                        <WhiteSpace />
+                        <input type="button" className="loginBtn" value="注册"/>
+                        </form>
+                    </div>
+
+
+                    <p className="xieyi"><input type="checkbox"/><span>我已经阅读并同意&nbsp;《服务协议》</span></p>
                 </div>
                 
             </main>
